79160ff7a2060556453ff4abe638d39b545a0e6b
Trip Pruitt <trippruitt@Trips-MacBook-Pro.local>
2015-09-22 13:59:10 -0400

    basic yeoman generator working

---
+++ generators/app/index.js
@@ -0,0 +1,25 @@
+
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend();
+
+
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  constructor: function () {
+    // Calling the super constructor is important so our generator is correctly set up
+    generators.Base.apply(this, arguments);
+
+    // Next, add your custom code
+    this.option('coffee'); // This method adds support for a `--coffee` flag
+  }
+});
+
+module.exports = generators.Base.extend({
+  method1: function () {
+    console.log('method 1 just ran');
+  },
+  method2: function () {
+    console.log('method 2 just ran');
+  }
+});
